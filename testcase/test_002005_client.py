aa0ce78a20469692b67249367866540675b35e38
dujian <575758259@qq.com>
2020-07-24 16:00:39 +0800

    客户管理最新

--- testcase/test_002005_client.py
+++ testcase/test_002005_client.py
@@ -1,28 +1,21 @@
 import unittest
-from data.read_data import read_excel
 from testcase.base_testcase import BaseTestCase
 from page.client_page import ClientPage
-
-from page.login_page import LoginPage
 
 class ClientTestCase(BaseTestCase):
     def test_client(self):
         '''新增客户-查看客户--删除'''
-        lp = LoginPage(self.driver )#实例化登录page的LoginPage类
-        lst_user = read_excel(r"../data/user.xlsx","user")#读取数据
-        result = lp.login(lst_user[0][0], lst_user[0][1])#调用登录login方法
-        self.assertIn(lst_user[0][0], result)
 
         lp = ClientPage(self.driver)
-        actual = lp.add("lisa8888","621601","niannian")
+        actual = lp.add("lisa8832188","621601","niannian")
         print(actual)
         self.assertIn("添加客户成功", actual)
 
         lp.check()
         lp.modification()
-        actual_a = lp.delete()
-        print(actual_a)
-        self.assertIn("删除成功",actual_a)
+        actual_result_delete = lp.delete()
+        print(actual_result_delete)
+        self.assertIn("删除成功",actual_result_delete)
 
 
 if __name__ == '__main__':
